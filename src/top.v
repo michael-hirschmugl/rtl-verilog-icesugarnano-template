dec90a0e3f65ae54a68c434bd11d028ddb656a80
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2025-09-02 13:13:14 +0200

    Fitted top.v to pcf file.

--- src/top.v
+++ src/top.v
@@ -1,13 +1,13 @@
 module top (
-    input  wire clk,    // 12 MHz vom Board
-    output wire LED
+    input  wire CLK_IN,    // 12 MHz vom Board
+    output wire GPIO_08
 );
     // LED-Blink (1 Hz aus 12 MHz)
     reg [23:0] div = 24'd0;
     reg        led = 1'b0;
-    assign LED = led;
+    assign GPIO_08 = led;
 
-    always @(posedge clk) begin
+    always @(posedge CLK_IN) begin
         div <= div + 1;
         if (div == 24'd6_000_000) begin
             led <= ~led;
